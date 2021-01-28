1987fd0f6a0b46c2356bd7e8881cf919afd2e8ef
Michał Zając <diuf95o@gmail.com>
2021-01-28 16:02:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def palidrony(word):
+    word_revers = ""
+    for i in range(len(word)-1,-1,-1):
+        word_revers += word[i]
+
+    if word_revers == word:
+        return True
+    else:
+        return False
+
+word =True
+
+while bool(word) == True:
+  word = input("give me your word\n")
+  if bool(word) == True:
+    print(palidrony(word))
